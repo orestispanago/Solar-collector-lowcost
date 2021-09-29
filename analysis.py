062f63b5c16998533548d10f1cfce8ddd93f1f8e
orestispanago <orestispanago@gmail.com>
2021-09-29 18:21:46 +0300

    Added sns.scatterplot for eff-dt_i plot

--- analysis.py
+++ analysis.py
@@ -2,11 +2,11 @@ import mysql.connector
 import datetime
 import pandas as pd
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 cp = 4200  # J/(kg*K)
 pyranometer_constant = 10.5  # uV/(W/m2)
-area_fpc = 1.3 * 0.66 # m
-area_sco4 = 0.275 * 0.275
+area = 1.3 * 0.66 # m
 
 day = datetime.date(2021, 9, 29)
 
@@ -34,15 +34,20 @@ def convertUnits(df):
     df["IRR"] = df["IRR"] * 1000 / pyranometer_constant  # mV to W/m2
 
 
-def plotEfficiencyDTI(df, x=None, y=None):
-    coly = df[y]
-    colx = df[x]
-    plt.plot(colx, coly, '.')
-    plt.ylabel("Efficiency")
-    plt.xlabel("(Tin - Tamb) / I")
-    plt.axvline(0, c='black', ls='--', linewidth=0.5)
-    # plt.axhline(0, c='black')
-    plt.axis([-0.005, 0.03, 0, 1])
+def plotEfficiencyDTI(df):
+    s = sns.scatterplot(data = df
+                    ,x = 'dt_i'
+                    ,y = 'eff'
+                    # ,edgecolor = 'none'
+                    ,alpha = .5
+                    # ,color = 'darkred'
+                    )
+    ax = s.axes
+    ax.axvline(0, c='black', ls='--', linewidth=0.5)
+    ax.set_ylabel('Efficiency')
+    ax.set_xlabel('(Tin - Tamb) / I')
+    ax.set_ylim(0, 1)
+    ax.set_xlim(-0.004, 0.03)
     plt.show()
 
 tin = select('IN')
@@ -56,12 +61,14 @@ df_list = [tin, tout, flow, irr, amb]
 df = pd.concat(df_list, axis=1)
 convertUnits(df)
 df = df.resample('1min').mean()
-df = df.loc[f"{day} 09:50:00":f"{day} 11:40:00"]
-df['eff'] = df['FLOW'] * cp * (df["OUT"] - df["IN"]) / (df["IRR"] * area_fpc)
+df = df.loc[f"{day} 09:50:00":f"{day} 11:50:00"]
+df['eff'] = df['FLOW'] * cp * (df["OUT"] - df["IN"]) / (df["IRR"] * area)
 
 df['eff'].plot()
 plt.show()
 
 df["dt_i"] = (df["IN"] - df['Tamb2']) / df['IRR']
-df = df.reset_index()
-plotEfficiencyDTI(df, x='dt_i', y='eff')
+# df = df.reset_index()
+plotEfficiencyDTI(df)
+
+

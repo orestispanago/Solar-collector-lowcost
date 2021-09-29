2771f48b3163b403b2e341429a6c970ae61931f1
Orestis <orestispanago@gmail.com>
2021-09-29 15:14:40 +0300

    Reading data from measurements_detailed table

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-Solar-collector-lowcost
+### Solar-collector-lowcost
+
+Selects data from the following table:
+
+| measurements_detailed |
+|-----------------------|
+| id                    |
+| time                  |
+| label                 |
+| count                 |
+| min                   |
+| max                   |
+| mean                  |
+| stdev                 |
+
+Need 1 query for each quantity:
+
+```SELECT time, mean FROM measurements_detailed WHERE label=%s```
--- analysis.py
+++ analysis.py
@@ -1,5 +1,6 @@
+import mysql.connector
 import datetime
-import datareader
+import pandas as pd
 import matplotlib.pyplot as plt
 
 cp = 4200  # J/(kg*K)
@@ -7,69 +8,60 @@ pyranometer_constant = 10.5  # uV/(W/m2)
 area_fpc = 1.3 * 0.66 # m
 area_sco4 = 0.275 * 0.275
 
-day = datetime.date(2021, 9, 15)
+day = datetime.date(2021, 9, 29)
 
+def select(label):
+    mydb = mysql.connector.connect(
+    host="DatabaseIP",
+    port="3360",
+    user="ReadOnlyUser",
+    passwd="ReadOnlyPassword",
+    database="collector"
+    )
+    mycursor = mydb.cursor()
+    mycursor.execute("""SELECT time, mean
+                     FROM measurements_detailed WHERE label=%s""",
+                     (label, ))
+    data = pd.DataFrame(mycursor.fetchall())
+    # data.columns = mycursor.column_names
+    data.columns = ['time', label]
+    data.set_index('time', inplace=True)
+    mydb.close()
+    return data
 
 def convertUnits(df):
-    df["Flow"] = df["Flow"] / 3600  # liters/h to litres/s
-    df["Irr"] = df["Irr"] * 1000 / pyranometer_constant  # mV to W/m2
+    df["FLOW"] = df["FLOW"] / 3600  # liters/h to litres/s
+    df["IRR"] = df["IRR"] * 1000 / pyranometer_constant  # mV to W/m2
 
 
-def plotEfficiencyDTI(df, x=None, y=None, label=None):
+def plotEfficiencyDTI(df, x=None, y=None):
     coly = df[y]
     colx = df[x]
-    plt.plot(colx, coly, '.', label=label)
+    plt.plot(colx, coly, '.')
     plt.ylabel("Efficiency")
     plt.xlabel("(Tin - Tamb) / I")
-    plt.axvline(0, c='black', ls='--')
-    plt.axhline(0, c='black')
+    plt.axvline(0, c='black', ls='--', linewidth=0.5)
+    # plt.axhline(0, c='black')
     plt.axis([-0.005, 0.03, 0, 1])
-    plt.legend()
+    plt.show()
 
+tin = select('IN')
+tout = select('OUT')
+amb = select('Tamb2')
+flow = select('FLOW')
+irr = select('IRR')
 
-df = datareader.selectDay(day)
+df_list = [tin, tout, flow, irr, amb]
+
+df = pd.concat(df_list, axis=1)
 convertUnits(df)
+df = df.resample('1min').mean()
+df = df.loc[f"{day} 09:50:00":f"{day} 11:40:00"]
+df['eff'] = df['FLOW'] * cp * (df["OUT"] - df["IN"]) / (df["IRR"] * area_fpc)
 
-irr = df["Irr"]
-flow = df["Flow"]
-tamb = df["Tenv2"]
-df["eff_fpc"] = flow * cp * (df["Tlout"] - df["Tlin"]) / (irr * area_fpc)
-df["eff_sco4"] = flow * cp * (df["Tsout"] - df["Tsin"]) / (irr * area_sco4)
-df["dt_i_fpc"] = (df["Tlin"] - tamb) / irr
-df["dt_i_sco4"] = (df["Tsin"] - tamb) / irr
-
-df.set_index("time", inplace=True)
-selected_eff_dti = df.loc[f"{day} 12:30:00": f"{day} 12:50:00"]
-
-
-plotEfficiencyDTI(selected_eff_dti, x="dt_i_fpc", y="eff_fpc", label="FPC")
-plotEfficiencyDTI(selected_eff_dti, x="dt_i_sco4", y="eff_sco4", label="SCO4")
+df['eff'].plot()
 plt.show()
 
-
-def plotDeltaTs(df):
-    dt_fpc = df["Tlout"] - df["Tlin"]
-    dt_fpc.name = "FPC"
-    dt_sco4 = df["Tsout"] - df["Tsin"]
-    dt_sco4.name = "SCO4"
-    dt_fpc.plot(legend=True)
-    dt_sco4.plot(ylabel="Tout - Tin (°C)", xlabel="Time, UTC", legend=True)
-    plt.show()
-
-
-def plotEfficiencies(df):
-    dt_fpc = df["eff_fpc"]
-    dt_fpc.name = "FPC"
-    dt_sco4 = df["eff_sco4"]
-    dt_sco4.name = "SCO4"
-    dt_fpc.plot(legend=True)
-    dt_sco4.plot(ylabel="Efficiency", xlabel="Time, UTC", legend=True)
-    plt.show()
-
-
-selected = df.loc[f"{day} 10:55:00":f"{day} 13:35:00"]
-# resampled = df.resample("10s").mean()
-
-
-plotDeltaTs(selected)
-plotEfficiencies(selected)
+df["dt_i"] = (df["IN"] - df['Tamb2']) / df['IRR']
+df = df.reset_index()
+plotEfficiencyDTI(df, x='dt_i', y='eff')
